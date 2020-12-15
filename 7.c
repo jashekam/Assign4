48c1c764991de9111469ddd71b6f010d2e40ce88
jashekam <75172324+jashekam@users.noreply.github.com>
2020-12-15 20:59:47 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int sumOfDigits(int num);
+void main()
+{
+    int n, sum;
+    
+    printf("Enter The Number :");
+    scanf("%d", &n);
+    sum = sumOfDigits(n);
+    printf("Sum of digits of %d = %d", n, sum);
+}
+
+int sumOfDigits(int num)
+{
+    if(num == 0)
+        return 0;
+        
+    return ((num % 10) + sumOfDigits(num / 10));
+}
