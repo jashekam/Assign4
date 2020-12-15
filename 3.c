9eee02397b2efeb57d64d24b9498a2fe8399d3a0
jashekam <75172324+jashekam@users.noreply.github.com>
2020-12-15 20:56:28 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int nat_sum(int l, int u);
+void main()
+{
+    int low, upp, sum;
+    printf("Enter the lower limit :");
+    scanf("%d",&low);
+    printf("Enter the upper limit :");
+    scanf("%d",&upp);
+    sum = nat_sum(low,upp);
+    printf(" Sum of Natural Numbers in given range =%d",sum);
+}
+
+int nat_sum(int l, int u)
+{
+    if(l==u)
+        return l;
+    else
+        return l + nat_sum(l+1, u);
+}
